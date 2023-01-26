32a6e0f4d6c0936b43b236a03d660be503fb5e9e
Neil1977 <Neil_lunn1977@hotmail.com>
2023-01-26 14:29:55 +0000

    completed all FREE Javascript DOM content

--- QuerydecedentsHTMLelements.js
+++ QuerydecedentsHTMLelements.js
@@ -76,3 +76,87 @@ const toggleColor = (isEntering) => {
 // type in your code here -**CAN USE MOUSELEAVE ALSO**
 element.addEventListener("mouseover", () => toggleColor(true));
 element.addEventListener("mouseout", () => toggleColor(false));
+
+// Remove element from the DOM
+// You may not see it in the example UI, but underneath the red circle is a green circle. Extend the function removeRedCircle to remove the circle with id red from the DOM.
+// Make sure that you really remove the element instead of just hiding it. Confirm that your code works by clicking the button.
+
+const button = document.querySelector("#button");
+
+const removeRedCircle = () => {
+  document.querySelector("#red").remove();
+};
+
+button.addEventListener("click", removeRedCircle);
+
+// Change id of HTML element
+// In this scenario the existing code listens to a click on the button. When the button is clicked, the function changeInput is triggered. changeInput tries to select an input field with id inputEl. But, the existing input field does not have this id. Add some Javascript code to add the id inputEl to the existing input field.
+// Verify that your code works by clicking the button.
+
+const button = document.querySelector("#wrapper button");
+
+const changeInput = () => {
+  const input = document.querySelector("#inputEl");
+  if (input) {
+    input.value = "Hello World";
+  }
+};
+
+button.addEventListener("click", changeInput);
+
+document.querySelector("#wrapper input").id = "inputEl";
+
+// Check the checkbox
+// Your first JavaScript DOM exercise. Let's start simple.
+// Extend the JavaScript code below to interact with the displayed HTML elements. Once you click the button, the checkbox should be checked.
+// Confirm your code by clicking the button!
+
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  // type in your code here
+  document.getElementById("checkbox").checked = true;
+});
+
+// Get full-name from inputs
+// Extend the JavaScript code below to interact with the displayed HTML elements.
+// This time we are looking for the full name. When the button is clicked, combine the names of the first two input fields. Insert the full name in the third input field.
+// Hint: Check if your code still works if you change the first or last name.
+// Confirm your code by clicking the button!
+
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  // type in your code here
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const fullName = document.getElementById("fullName");
+  fullName.value = firstName.value + " " + lastName.value;
+});
+
+// Stop and restart the moving button
+// This is a good exercise to learn about recursive functions. The function move in the code below moves the button 1px to the left or the right. It is recursive because it calls itself again and again. This keeps the button moving.
+// Extend the JavaScript code. Once you click the button, it should stop moving. When you click it again, it should move again.
+// Confirm your code by clicking the button twice.
+
+const button = document.getElementById("button");
+let stopped = false;
+
+function move(isReturning) {
+  const width = button.parentNode.clientWidth;
+  const left = parseInt(button.style.left, 10) || 0;
+  if (!stopped) {
+    button.style.left = (isReturning ? left - 1 : left + 1) + "px";
+    setTimeout(
+      () =>
+        move((isReturning && left > 0) || left === width - button.clientWidth),
+      10
+    );
+  }
+}
+
+move();
+
+button.addEventListener("click", () => {
+  // type in your code here
+  stopped = !stopped;
+  move();
+});
